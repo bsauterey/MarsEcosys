6ef455f94d714e449838b7c3cc1cb9d196545126
Sauterey Boris <bsauterey@gmail.com>
2022-07-29 14:30:07 +0200

    The launcher is know commented
    The launcher is know commented

--- Launcher_dyn_coupling_rho.py
+++ Launcher_dyn_coupling_rho.py
@@ -14,6 +14,9 @@ from scipy.integrate import solve_ivp
 import sys
 from T_to_rho_and_Tsurf import *
 
+
+## Sets environmental variables and parameters
+# Atmosphere
 p = np.random.uniform(0.5,3)*10**5
 pCH4           = 1e-4
 pH2            = np.random.uniform(3e-3,0.1)-2*pCH4
@@ -25,45 +28,46 @@ Hatm           = pH2  * ntot * 1e-6
 Gatm           = pCH4 * ntot * 1e-6
 N2atm          = pN2  * ntot * 1e-6
 COatm          = pCO  * ntot * 1e-6
-T_freez        = 252    #Can be 203, 252, or 273
 ntot           = (p*S*1e-4)/(g*(pCO2*M_CO2+pN2*M_N2+pCH4*M_CH4+pH2*M_H2))
 
-a_eps          = np.random.uniform(0.2,0.6)
-a_to           = np.random.uniform(1.5,2.5)
-r_surf         = np.exp(np.random.uniform(log(1e-4),log(1e-3)))
+# Brines' freezing point
+T_freez        = 252    #Can be 203, 252, or 273
 
+# Initial average T°, ice-free portion of the surface, and T° in that region
 T_av           = T_func(p*1e-5,pH2,pCH4)
-lat_freez      = min(max(np.arccos(max(min((T_freez-T_av+5*pi)/20,1),-1))*180/pi,0),90)
 rho            = float(T_to_rho252(T_av)) #Can be T_to_rho203, T_to_rho252, or T_to_rho273
 T_surf         = max(T_av,float(T_to_Tsurf252(T_av))) #Can be T_to_Tsurf203, T_to_Tsurf252, or T_to_Tsurf273
 
+# Soil properties
+a_eps          = np.random.uniform(0.2,0.6)
+a_to           = np.random.uniform(1.5,2.5)
+r_surf         = np.exp(np.random.uniform(log(1e-4),log(1e-3)))
 ztot           = np.random.uniform(5,20)
 layers         = 100
 a_T            = np.random.uniform(10,30)
-
 z              = ztot
 Ti             = T_surf + a_T * ztot
 z_vec          = np.linspace(0,ztot,layers)
 T_vec          = T_surf + a_T * z_vec
+par            = [a_eps,a_to,r_surf]
 
+# Fluxes in and out of the atmosphere
 E              = 1.6e13
 Esc            = E*(pH2+pCH4/2)
 F_CH4          = float(FCH4_func(pH2,pCH4))
 F_H2           = float(FH2_func(pH2,pCH4))
 Volc           = Esc - F_H2
-par            = [a_eps,a_to,r_surf]
 
-
-print(T_av, lat_freez, rho, T_surf)
-
-pickle.dump([p,pH2,pCO2,pCH4,a_eps,a_to,r_surf,T_av,ztot,a_T],open(sys.argv[1]+'.params','wb'))
-
+# Vector containing the environmental variables
 Env            = [pH2*p*1e-5*alphaH*1e-3*Av,pCO2*p*1e-5*np.exp(9345.17/T_surf-167.8108+23.3585*np.log(T_surf)+(0.023517-2.3656e-4*T_surf+4.7036e-7*T_surf**2)*35.0)*1e-3*Av,pCH4*p*1e-5*alphaG*1e-3*Av,pN2*p*1e-5*alphaN2*1e-3*Av]
 
+## Sets the initial conditions and shape them for the numerical solver
+# Environmental variables
 init_chem      = []
 for i in np.arange(len(Env)):
 	init_chem += [Env[i] for j in np.arange(layers)]
 
+# Biological variables
 FTnames        = ['meth','NO3metht','NO2metht','H2SO4metht','acet','acet2','acett','ferm','photoH2']
 FTlist         = {'meth':True,'NO3metht':False,'NO2metht':False,'H2SO4metht':False,'acet':False,'acet2':False,'acett':False,'ferm':False,'photoH2':False}
 t_intro_list   = {'meth':1e100,'NO3metht':1e100,'NO2metht':1e100,'H2SO4metht':1e100,'acet':1e100,'acet2':1e100,'acett':1e100,'ferm':1e100,'photoH2':1e100}
@@ -85,7 +89,11 @@ for i in FTnames:
 			#traits   = Run_def_FT(i,T_vec[j],Forced_rc=True)
 			init_X0 += [traits[9]*10]
 
+# Total vector (environment then cell count then cell biomass content)
 init = np.array(init_chem + init_NC + init_X0)
+
+## Saves the values of the main parameters of the simulation under the name that must be provided as argument
+pickle.dump([p,pH2,pCO2,pCH4,a_eps,a_to,r_surf,T_av,ztot,a_T],open(sys.argv[1]+'.params','wb'))
 
 der = stepfunc(0,0,init,layers,FTnames,FTlist,traits_list,Env,z_vec,T_vec,par,ztot,p)
 
@@ -125,15 +133,15 @@ DiffGT  = [0]
 t       = [0]
 
 t_count = 0
-ratchet_step = 1000
+ratchet_step = 1000          # Determines how frequently the state of the system is saved
 ratchet = ratchet_step
 
 #tmax = 365*300
-#tmax = 365*400
-tmax = 365*1
+tmax = 365*400
+#tmax = 365*1
 #tmax = 10000
-if rho1 > 0:
-	while t[-1] < tmax:
+if rho1 > 0:                 # Check whether Mars is initially habitable or not
+	while t[-1] < tmax:      # Time loop
 
 		y0        = y1
 		Env0       = Env1
@@ -161,10 +169,10 @@ if rho1 > 0:
 		der = stepfunc(t_count,ratchet,y0,layers,FTnames,FTlist,traits_list,Env0,z_vec,T_vec0,par,ztot,p0)
 		no_zero  = np.where(np.array(der) != 0)
 		if t[-1] < 2000:
-			dt   = max(min(abs(y0[no_zero]/np.array(der)[no_zero]))/10,1e-2)
+			dt   = max(min(abs(y0[no_zero]/np.array(der)[no_zero]))/10,1e-2)    # Adaptive time step (must be low at first)
 		else:
 			#dt   = min(abs(y0[no_zero]/np.array(der)[no_zero]))/10
-			dt   = max(min(abs(y0[no_zero]/np.array(der)[no_zero]))/5,1e-1)
+			dt   = max(min(abs(y0[no_zero]/np.array(der)[no_zero]))/5,1e-1)     # Adaptive time step
 		if t[-1] + dt > tmax: dt = tmax - t[-1]
 		## New state
 		y1             = y0 + np.array(der) * dt
@@ -195,7 +203,6 @@ if rho1 > 0:
 		pN2_1  = (1-(pCH4_1+pH2_1+pCO))*0.05
 
 		T_av1       = T_func(p*1e-5,pH2_1,pCH4_1)
-		lat_freez1 = min(max(np.arccos(max(min((T_freez-T_av1+5*pi)/20,1),-1))*180/pi,0),90)
 		rho1       = float(T_to_rho252(T_av1))    #Can be T_to_rho203, T_to_rho252, or T_to_rho273
 		T_surf1    = max(T_av1,float(T_to_Tsurf252(T_av1)))    #Can be T_to_Tsurf203, T_to_Tsurf252, or T_to_Tsurf273
 		T_vec1     = T_surf1 + a_T * z_vec
@@ -266,6 +273,7 @@ if rho1 > 0:
 t_vec = np.append(np.arange(0,max(t),1000),max(t))
 Slice = np.unique([np.argmin(abs(t-i)) for i in t_vec])
 
+## Saves the output under the name that must be provided as argument
 if len(Slice) <= 1:
 	f = gzip.open(sys.argv[1]+'.results_dc','wb')
 	pickle.dump([np.array(t),np.array(y),np.array(pT),np.array(T_surfT),np.array(T_avT),np.array(rhoT),np.array(pCH4T),np.array(pH2T),np.array(pCO2T),np.array(pN2T),np.array(DiffHT),np.array(DiffGT)],f)
